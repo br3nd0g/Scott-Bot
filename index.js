343072289219cdb73ff4ff584afee8b0cb1b3aac
John Doe <johndoe@email.com>
2023-05-16 20:42:04 +0100

    done i believe

--- index.js
+++ index.js
@@ -1,28 +1,59 @@
 require('dotenv').config();
 const { Client, Events, GatewayIntentBits, ActivityType } = require('discord.js');
 
-const client = new Client({ intents: [
-    GatewayIntentBits.DirectMessages,
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildBans,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent,
-    GatewayIntentBits.GuildMessageReactions
-] });
+const targetServer = '1082296054512369789';
+const targetMember = '1015588653164142663';
+const targetChannel = '1082296055317667923';
+let targetObj;
+let scottOnline = false
+const messages = ["Scott is here and ready to party!", "Scott is now online! Hooray!", "Scott is online, yippeeeeeeeeeeeeee"]
+
+const client = new Client({ intents: 33539 });
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
-    console.log(client.users);
 
     client.user.setPresence({
         activities: [{ name: `With Toys`, type: ActivityType.Playing }],
         status: 'online'
     });
+
+});
+
+client.on('messageCreate', async (message) => {
+    
 });
 
 client.login(process.env.clToken);
 
-
 setInterval(function() {
     // check if scott user id status is offline, if not, send message going "@everyone scott online"
-}, 30 * 1000); 
+    let scottStatus = statusCheck()
+    statusComparison(scottStatus)
+    console.log(scottStatus)
+    console.log("looper")
+
+}, 15 * 1000); 
+
+function alertEveryone(){
+    let randomIndex = Math.floor(Math.random() * messages.length);
+
+    const channel = client.channels.cache.get(targetChannel);
+    channel.send(`@everyone ${messages[randomIndex]}`);
+}
+
+function statusCheck(){
+    //gets guild object of the guild, gets userpresence object of user, gets status
+    const guild = client.guilds.resolve(targetServer);
+    const userPresence = guild.presences.resolve(targetMember)
+    return userPresence.status
+}
+
+function statusComparison(status){
+    if(scottOnline === false && (status === "dnd" || status === "online")){
+        scottOnline = true
+        alertEveryone()
+    } else if (scottOnline === true && status === "offline"){
+        scottOnline = false
+    }
+}
